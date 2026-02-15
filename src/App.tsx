f886c9053c014ccaff0d29d75208f7fd2cb00124
Loai Wael <loaiwael.dev@gmail.com>
2026-02-15 20:59:25 +0200

    UPD app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { AdminProvider } from "./contexts/AdminContext";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const App = () => {
   return (
-    <div className="text-brand-error bg-brand-primary">App</div>
-  )
-}
+    <AdminProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
+    </AdminProvider>
+  );
+};
 
-export default App
+export default App;
